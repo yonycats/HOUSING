1a6d2d4b8ce17c1ee75ca090318ad9cb481e13b0
yonycats <dusgml1953@gmail.com>
2024-05-14 09:55:09 +0900

    프린트 수정

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -23,7 +23,6 @@ public class MainController extends Print {
 	MemberService memberService = MemberService.getInstance();
 	NoticeService noticeService = NoticeService.getInstance();
 	EstateService estateService = EstateService.getInstance();
-	qqqqqqqqqqqqqq
 
 	boolean debug = true;
 
@@ -33,6 +32,7 @@ public class MainController extends Print {
 
 	private void start() {
 		View view = View.HOME;
+		System.out.println("제발 돼라 어ㅓㅇ엉");
 		while (true) {
 			switch (view) {
 			case HOME:
